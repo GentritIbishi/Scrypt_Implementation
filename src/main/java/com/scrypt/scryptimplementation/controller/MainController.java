dd71b9e6cb519057b6035122b7260c3a742abf94
Festina Qorrolli <festinaqorrolli@hotmail.com>
2023-11-18 23:54:39 +0100

    controller update

--- src/main/java/com/scrypt/scryptimplementation/controller/MainController.java
+++ src/main/java/com/scrypt/scryptimplementation/controller/MainController.java
@@ -16,7 +16,7 @@ import javafx.stage.Stage;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import static com.scrypt.scryptimplemenation.utils.Constants.*;
+import static com.scrypt.scryptimplementation.utils.Constants.*;
 
 public class MainController implements Initializable {
     private double x, y;
@@ -72,17 +72,32 @@ public class MainController implements Initializable {
                     lab.setStyle("-fx-text-fill: #FF073A;");
                 }
                 else {
-                    generatedSecuredPasswordHash = SCryptUtil.scrypt(plaintextField.getText(),
+
+                    // Generate a secure hash of the plaintext password using SCrypt algorithm
+                    generatedSecuredPasswordHash = SCryptUtil.scrypt(
+
+                            // Get the plaintext password from the text field
+                            plaintextField.getText(),
                             Integer.parseInt(nField.getText()),
                             Integer.parseInt(rField.getText()),
-                            Integer.parseInt(pField.getText()));
+                            Integer.parseInt(pField.getText())
+                    );
 
-                    // show the hashPassword to tView
+                    // Display the generated hash password in hashGeneratedField
                     hashGeneratedField.setText(generatedSecuredPasswordHash);
+
+                    // Enable editing of hashGeneratedField
                     hashGeneratedField.setEditable(true);
+
+                    // Disable disablement of hashGeneratedField
                     hashGeneratedField.setDisable(false);
+
+                    // Update a label to show success message for hash generation
                     lab.setText("Hash Password has generated successfully!");
+
+                    // Change style of the label to green indicating success
                     lab.setStyle("-fx-text-fill: #00FF00;");
+
                 }
             }catch (Exception e) {
                 lab.setText(STATUS + ": "+e.getMessage());
@@ -108,32 +123,67 @@ public class MainController implements Initializable {
             lab.setStyle("-fx-text-fill: #FF073A;");
         } else {
             try {
-                // do the checking
-                boolean matched = SCryptUtil.check(plaintextCheckField.getText(), hashGeneratedField.getText());
+                // Check if the entered plaintext matches the generated hash
+
+                boolean matched = SCryptUtil.check(
+
+                        // Get the entered plaintext password
+                        plaintextCheckField.getText(),
+
+                        // Get the previously generated hash password
+                        hashGeneratedField.getText()
+                );
+
+                // If the plaintext matches the hash
                 if (matched) {
+
+                    // Set the booleanResultField to TRUE
                     booleanResultField.setText(TRUE);
+
+                    // Style the booleanResultField for a positive match
                     booleanResultField.setStyle("    -fx-border-color: #00FF00;\n" +
                             "    -fx-background-color: #00FF00;\n" +
                             "    -fx-border-radius: 0;\n" +
                             "    -fx-border-width: 2px;\n" +
                             "    -fx-text-fill: #000;\n" +
                             "    -fx-prompt-text-fill: #58656D;");
+
+                    // Update the label with a status message for a successful match
                     lab.setText(STATUS_TRUE);
+
+                    // Change the style of the label to indicate a successful match
                     lab.setStyle("-fx-text-fill: #19ea19;");
+
                 } else {
+                    // If the plaintext does not match the hash
+
+                    // Set the booleanResultField to FALSE
                     booleanResultField.setText(FALSE);
+
+                    // Style the booleanResultField for a failed match
                     booleanResultField.setStyle(" -fx-border-color: red;\n" +
                             "    -fx-background-color: red;\n" +
                             "    -fx-border-radius: 0;\n" +
                             "    -fx-border-width: 2px;\n" +
                             "    -fx-text-fill: #000;\n" +
                             "    -fx-prompt-text-fill: #000;");
+
+                    // Update the label with a status message for a failed match
                     lab.setText(STATUS_FALSE);
+
+                    // Change the style of the label to indicate a failed match
                     lab.setStyle("-fx-text-fill: #ec0b0b;");
                 }
+
+                // Make the booleanResultField uneditable
                 booleanResultField.setEditable(false);
+
+                // Disable the booleanResultField
                 booleanResultField.setDisable(true);
+
+                // Set the cursor of booleanResultField to Hand cursor
                 booleanResultField.setCursor(Cursor.HAND);
+
             }catch (Exception e) {
                 lab.setText(STATUS + ": "+e.getMessage());
             }
---
+++ src/main/java/com/scrypt/scryptimplementation/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.scrypt.scryptimplementation.utils;
+
+public class Constants {
+    public static String STATUS = "STATUS";
+    public static String TRUE = "TRUE";
+    public static String FALSE = "FALSE";
+    public static String STATUS_TRUE = "Status: Success!";
+    public static String STATUS_FALSE = "Status: Failed!";
+}
